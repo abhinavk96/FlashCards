ed15e4b455568185a7b6a16505a5ad4db43a5603
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-17 02:32:40 +0530

    Remove old files

--- app/src/Config/Firebase/db_config.example.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "<API-KEY>",
-    authDomain: "<DOMAIN>",
-    databaseURL: "<URL>",
-    projectId: "<PROJECT-ID>",
-    storageBucket: "",
-    messagingSenderId: "<ID>"
-}
