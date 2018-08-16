765f6b30e8c65f036e1893f17764f2782b70fa42
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-17 02:32:07 +0530

    Relocating app

---
+++ src/Config/Firebase/db_config.example.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "<API-KEY>",
+    authDomain: "<DOMAIN>",
+    databaseURL: "<URL>",
+    projectId: "<PROJECT-ID>",
+    storageBucket: "",
+    messagingSenderId: "<ID>"
+}
