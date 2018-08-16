c05e075395197c0b6625b847f8fae702027326ad
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-17 02:45:08 +0530

    Make config visible for now

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyBNSnO3i8vHkGEwfo03VBpP1TjwtKqHsfo",
+    authDomain: "flashcards-da9aa.firebaseapp.com",
+    databaseURL: "https://flashcards-da9aa.firebaseio.com",
+    projectId: "flashcards-da9aa",
+    storageBucket: "",
+    messagingSenderId: "424332427479"
+}
